d61ed47b040a1faef83d4ea1c1047c3ed03ec573
Faisal Hakim <159868796+faisalHakim11@users.noreply.github.com>
2025-08-05 00:13:07 +0530

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('ratingdb', 'postgres', 'postgres', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-module.exports = sequelize;
